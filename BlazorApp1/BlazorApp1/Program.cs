03492e21ac29612eed25c0ec168d6e3df90a3b81
Al Nixon <raven_darknight@hotmail.com>
2022-02-12 11:08:08 -0600

    wip: added simple dialog component

--- BlazorApp1/BlazorApp1/Program.cs
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp1.Data;
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazoredModal();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
